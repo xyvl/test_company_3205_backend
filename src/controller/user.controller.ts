939ad7f4c20f51e02885c7b1a511b9addbce2d1b
Vlad Podshivalov <23091980luba@gmail.com>
2023-06-15 13:44:11 +0500

    End

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,7 +5,8 @@ interface IReq {
 }
 class user {
   async getUser(req: Request<any, any, IReq>, res: Response) {
-      const { email, number } = req.body;
+      setTimeout(() => {
+        const { email, number } = req.body;
       const data = [
         {
           email: "jim@gmail.com",
@@ -49,6 +50,7 @@ class user {
       else if (data.some((el) => el.number === number))
         return res.json("Есть пользователь с таким номером");
       else return res.status(404).json("Такого пользователя нет");
+      }, 5000)
     }
 }
 export default new user();
